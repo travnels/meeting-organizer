4ccc3c1ae9dec8a4783668d1d2727685a80a11ec
Mike Szwedo <mike.szwedo@gmail.com>
2012-09-10 23:35:08 -0600

    Hard coded design for first DJUG

---
+++ index.js
@@ -0,0 +1,44 @@
+
+$(document).ready(function() {
+
+
+$(function() {
+	$(".meter > span").each(function() {
+		$(this)
+			.data("origWidth", $(this).width())
+			.width(0)
+			.animate({
+				width: $(this).data("origWidth")
+			}, 1200);
+	});
+});
+
+
+setInterval( function() {
+	// Create a newDate() object and extract the seconds of the current time on the visitor's
+	var seconds = new Date().getSeconds();
+	// Add a leading zero to seconds value
+	$("#sec").html(( seconds < 10 ? "0" : "" ) + seconds);
+	},1000);
+	
+setInterval( function() {
+	// Create a newDate() object and extract the minutes of the current time on the visitor's
+	var minutes = new Date().getMinutes();
+	// Add a leading zero to the minutes value
+	$("#min").html(( minutes < 10 ? "0" : "" ) + minutes);
+    },1000);
+	
+setInterval( function() {
+	// Create a newDate() object and extract the hours of the current time on the visitor's
+	var hours = new Date().getHours();
+	// Add a leading zero to the hours value
+	if (hours < 10)
+		hours=0+hours;
+	else
+		hours=hours-12;
+		
+	$("#hours").html(hours);
+	//$("#hours").html(( hours < 10 ? "0" : "" ) + hours);
+    }, 1000);	
+		
+});
