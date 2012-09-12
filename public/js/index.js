02fec580448690b764381214ab68d8b5801cdd18
tnelson <travis.o.nelson@gmail.com>
2012-09-12 01:07:13 -0600

    condensed code to not have 3 separate intervals and fixed 0 hour problem
    0 hour is between midnight and 1am.  Should read as 12:XX

--- public/js/index.js
+++ public/js/index.js
@@ -1,44 +1,30 @@
-
 $(document).ready(function() {
 
+    $(function() {
+        $(".meter > span").each(function() {
+            $(this)
+                .data("origWidth", $(this).width())
+                .width(0)
+                .animate({
+                    width: $(this).data("origWidth")
+                }, 1200);
+        });
+    });
 
-$(function() {
-	$(".meter > span").each(function() {
-		$(this)
-			.data("origWidth", $(this).width())
-			.width(0)
-			.animate({
-				width: $(this).data("origWidth")
-			}, 1200);
-	});
-});
+    setInterval(function() {
+        var date = new Date(),
+            seconds = date.getSeconds(),
+            minutes = date.getMinutes(),
+            hours = date.getHours();
 
+        if (hours == 0)
+            hours = 12; //account for 0 hour, which is 12
+        else if (hours > 12)
+            hours = hours - 12; //keep in 12 hour format
 
-setInterval( function() {
-	// Create a newDate() object and extract the seconds of the current time on the visitor's
-	var seconds = new Date().getSeconds();
-	// Add a leading zero to seconds value
-	$("#sec").html(( seconds < 10 ? "0" : "" ) + seconds);
-	},1000);
-	
-setInterval( function() {
-	// Create a newDate() object and extract the minutes of the current time on the visitor's
-	var minutes = new Date().getMinutes();
-	// Add a leading zero to the minutes value
-	$("#min").html(( minutes < 10 ? "0" : "" ) + minutes);
-    },1000);
-	
-setInterval( function() {
-	// Create a newDate() object and extract the hours of the current time on the visitor's
-	var hours = new Date().getHours();
-	// Add a leading zero to the hours value
-	if (hours <= 12)
-		hours=0+hours;
-	else
-		hours=hours-12;
-		
-	$("#hours").html(hours);
-	//$("#hours").html(( hours < 10 ? "0" : "" ) + hours);
-    }, 1000);	
-		
+        $("#sec").html(( seconds < 10 ? "0" : "" ) + seconds); //add leading 0
+        $("#min").html(( minutes < 10 ? "0" : "" ) + minutes); //add leading 0
+        $("#hours").html(hours);
+    }, 1000);
+
 });
